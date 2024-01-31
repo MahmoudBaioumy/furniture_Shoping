f8c992c5ffcae992968583125d8b21563750905d
MahmoudBaioumy <mbaioumy84@gmail.com>
2024-01-31 02:52:10 +0200

    new

--- lib/features/Home/fav.dart
+++ lib/features/Home/fav.dart
@@ -11,92 +11,329 @@ class fav extends StatefulWidget {
 }
 
 class _favState extends State<fav> {
-  List<usersmodel> users = [
-    usersmodel(
-      type: 'Coffe Table',
-      image: 'assets/frist.png',
-      salary: '50.00',
-    ),
-    usersmodel(
-      type: 'Coffe Chair',
-      image: 'assets/second.png',
-      salary: '50.00',
-    ),
-    usersmodel(
-      type: 'Minimal Stand',
-      image: 'assets/third.png',
-      salary: '50.00',
-    ),
-    usersmodel(
-      type: 'Minimal Desk',
-      image: 'assets/fourt.png',
-      salary: '50.00',
-    ),
-    usersmodel(
-      type: 'Minimal lamp',
-      image: 'assets/fifet.png',
-      salary: '50.00',
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading:
-              IconButton(onPressed: () {}, icon: Icon(Icons.search_rounded)),
-          title: Text(
-            'favorites',
-            style: getTitelStyle(
-                fontSize: 16, fontWeight: FontWeight.w700, color: Colors.black),
-          ),
-          centerTitle: true,
-          actions: [
-            IconButton(
-                onPressed: () {}, icon: Icon(Icons.shopping_cart_outlined))
-          ],
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.search_rounded)),
+        title: Text(
+          'favorites',
+          style: getTitelStyle(
+              fontSize: 16, fontWeight: FontWeight.w700, color: Colors.black),
         ),
-        body: ListView.separated(
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: SizedBox(
-                  height: 150,
-                  width: 100,
-                  child: ClipRRect(
-                    child: Image.asset(
-                      users[index].image,
-                      fit: BoxFit.cover,
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart_outlined))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              //frist
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 120,
+                    width: 120,
+                    decoration: BoxDecoration(
+                        color: appColors.whitecolor,
+                        borderRadius:
+                            BorderRadiusDirectional.all(Radius.circular(50)),
+                        image: DecorationImage(
+                            image: AssetImage('assets/frist.png'))),
+                  ),
+                  Gap(4),
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'coffee Table',
+                          style: getSmalStyle(
+                              fontWeight: FontWeight.w600,
+                              color: appColors.grey2color),
+                        ),
+                        Text(
+                          '50.00',
+                          style: getBodyStyle(
+                              fontWeight: FontWeight.w600,
+                              color: appColors.balckcolor),
+                        ),
+                      ],
                     ),
                   ),
+                  Spacer(),
+                  Column(
+                    children: [
+                      IconButton(
+                          onPressed: () {}, icon: Icon(Icons.cancel_outlined)),
+                      Gap(20),
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.shopping_bag_outlined)),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Divider(),
+            //secound
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      color: appColors.whitecolor,
+                      borderRadius:
+                          BorderRadiusDirectional.all(Radius.circular(50)),
+                      image: DecorationImage(
+                          image: AssetImage('assets/second.png'))),
                 ),
-                title: Text(
-                  users[index].type,
-                  style: getTitelStyle(
-                      fontSize: 19,
-                      fontWeight: FontWeight.w600,
-                      color: appColors.graycolor),
+                Gap(4),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'coffee Chair',
+                        style: getSmalStyle(
+                            fontWeight: FontWeight.w600,
+                            color: appColors.grey2color),
+                      ),
+                      Text(
+                        '20.00',
+                        style: getBodyStyle(
+                            fontWeight: FontWeight.w600,
+                            color: appColors.balckcolor),
+                      ),
+                    ],
+                  ),
                 ),
-                subtitle: Text(
-                  users[index].salary,
-                  style: getSmalStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                      color: appColors.balckcolor),
-                ),
-                trailing: Column(
+                Spacer(),
+                Column(
                   children: [
-                    // IconButton(
-                    //      onPressed: () {}, icon: Icon(Icons.cancel_outlined)),
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.cancel_outlined)),
+                    Gap(20),
                     IconButton(
                         onPressed: () {},
                         icon: Icon(Icons.shopping_bag_outlined)),
                   ],
+                )
+              ],
+            ),
+            Divider(),
+            //third
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      color: appColors.whitecolor,
+                      borderRadius:
+                          BorderRadiusDirectional.all(Radius.circular(50)),
+                      image: DecorationImage(
+                          image: AssetImage('assets/third.png'))),
                 ),
-              );
-            },
-            separatorBuilder: (context, index) {
-              return Divider();
-            },
-            itemCount: users.length));
+                Gap(4),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Minimal Stand',
+                        style: getSmalStyle(
+                            fontWeight: FontWeight.w600,
+                            color: appColors.grey2color),
+                      ),
+                      Text(
+                        '25.00',
+                        style: getBodyStyle(
+                            fontWeight: FontWeight.w600,
+                            color: appColors.balckcolor),
+                      ),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                Column(
+                  children: [
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.cancel_outlined)),
+                    Gap(20),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.shopping_bag_outlined)),
+                  ],
+                )
+              ],
+            ),
+            Divider(),
+            //four
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      color: appColors.whitecolor,
+                      borderRadius:
+                          BorderRadiusDirectional.all(Radius.circular(50)),
+                      image: DecorationImage(
+                          image: AssetImage('assets/fourt.png'))),
+                ),
+                Gap(4),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Minimal Desk',
+                        style: getSmalStyle(
+                            fontWeight: FontWeight.w600,
+                            color: appColors.grey2color),
+                      ),
+                      Text(
+                        '50.00',
+                        style: getBodyStyle(
+                            fontWeight: FontWeight.w600,
+                            color: appColors.balckcolor),
+                      ),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                Column(
+                  children: [
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.cancel_outlined)),
+                    Gap(20),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.shopping_bag_outlined)),
+                  ],
+                )
+              ],
+            ),
+            Divider(),
+            //five
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      color: appColors.whitecolor,
+                      borderRadius:
+                          BorderRadiusDirectional.all(Radius.circular(50)),
+                      image: DecorationImage(
+                          image: AssetImage('assets/fifet.png'))),
+                ),
+                Gap(4),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Minimal Lamp',
+                        style: getSmalStyle(
+                            fontWeight: FontWeight.w600,
+                            color: appColors.grey2color),
+                      ),
+                      Text(
+                        '12.00',
+                        style: getBodyStyle(
+                            fontWeight: FontWeight.w600,
+                            color: appColors.balckcolor),
+                      ),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                Column(
+                  children: [
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.cancel_outlined)),
+                    Gap(20),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.shopping_bag_outlined)),
+                  ],
+                )
+              ],
+            ),
+            Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      color: appColors.whitecolor,
+                      borderRadius:
+                          BorderRadiusDirectional.all(Radius.circular(50)),
+                      image: DecorationImage(
+                          image: AssetImage('assets/frist.png'))),
+                ),
+                Gap(4),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'coffee Table',
+                        style: getSmalStyle(
+                            fontWeight: FontWeight.w600,
+                            color: appColors.grey2color),
+                      ),
+                      Text(
+                        '50.00',
+                        style: getBodyStyle(
+                            fontWeight: FontWeight.w600,
+                            color: appColors.balckcolor),
+                      ),
+                    ],
+                  ),
+                ),
+                Spacer(),
+                Column(
+                  children: [
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.cancel_outlined)),
+                    Gap(20),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.shopping_bag_outlined)),
+                  ],
+                )
+              ],
+            ),
+            Divider(),
+          ],
+        ),
+      ),
+    );
   }
 }
